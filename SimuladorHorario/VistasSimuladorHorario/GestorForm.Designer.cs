7c89fa045b7ce31f58996732677cd97db1ddad7e
= <=>
2018-05-11 00:41:06 -0300

    Pedroru

--- SimuladorHorario/VistasSimuladorHorario/GestorForm.Designer.cs
+++ SimuladorHorario/VistasSimuladorHorario/GestorForm.Designer.cs
@@ -29,23 +29,37 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(114, 62);
+            this.button1.Location = new System.Drawing.Point(86, 50);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 52);
+            this.button1.Size = new System.Drawing.Size(136, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Crear Curso";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar Curso";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // GestorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GestorForm";
             this.Text = "GestorForm";
             this.ResumeLayout(false);
@@ -55,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
