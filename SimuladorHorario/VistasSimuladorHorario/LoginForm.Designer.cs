215eea65b5955618eb722b7cd71d0c4cab07bcce
macanepa <macanepa@miuandes.cl>
2018-05-10 23:23:09 -0300

    ahora si q ditto

--- SimuladorHorario/VistasSimuladorHorario/LoginForm.Designer.cs
+++ SimuladorHorario/VistasSimuladorHorario/LoginForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.usuarioText = new System.Windows.Forms.TextBox();
             this.usuarioLabel = new System.Windows.Forms.Label();
             this.contraseñaLabel = new System.Windows.Forms.Label();
@@ -41,49 +42,44 @@
             // 
             this.usuarioText.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.usuarioText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.usuarioText.Location = new System.Drawing.Point(241, 169);
-            this.usuarioText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.usuarioText.Location = new System.Drawing.Point(181, 137);
             this.usuarioText.Name = "usuarioText";
-            this.usuarioText.Size = new System.Drawing.Size(285, 22);
+            this.usuarioText.Size = new System.Drawing.Size(214, 20);
             this.usuarioText.TabIndex = 0;
             this.usuarioText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // usuarioLabel
             // 
             this.usuarioLabel.AutoSize = true;
-            this.usuarioLabel.Location = new System.Drawing.Point(164, 169);
-            this.usuarioLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.usuarioLabel.Location = new System.Drawing.Point(123, 137);
             this.usuarioLabel.Name = "usuarioLabel";
-            this.usuarioLabel.Size = new System.Drawing.Size(57, 17);
+            this.usuarioLabel.Size = new System.Drawing.Size(43, 13);
             this.usuarioLabel.TabIndex = 1;
             this.usuarioLabel.Text = "Usuario";
             // 
             // contraseñaLabel
             // 
             this.contraseñaLabel.AutoSize = true;
-            this.contraseñaLabel.Location = new System.Drawing.Point(152, 215);
-            this.contraseñaLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.contraseñaLabel.Location = new System.Drawing.Point(114, 175);
             this.contraseñaLabel.Name = "contraseñaLabel";
-            this.contraseñaLabel.Size = new System.Drawing.Size(81, 17);
+            this.contraseñaLabel.Size = new System.Drawing.Size(61, 13);
             this.contraseñaLabel.TabIndex = 3;
             this.contraseñaLabel.Text = "Contraseña";
             // 
             // contraseñaText
             // 
-            this.contraseñaText.Location = new System.Drawing.Point(241, 215);
-            this.contraseñaText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.contraseñaText.Location = new System.Drawing.Point(181, 175);
             this.contraseñaText.Name = "contraseñaText";
-            this.contraseñaText.Size = new System.Drawing.Size(284, 22);
+            this.contraseñaText.Size = new System.Drawing.Size(214, 20);
             this.contraseñaText.TabIndex = 2;
             this.contraseñaText.UseSystemPasswordChar = true;
             this.contraseñaText.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // ingresarButton
             // 
-            this.ingresarButton.Location = new System.Drawing.Point(333, 258);
-            this.ingresarButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ingresarButton.Location = new System.Drawing.Point(250, 210);
             this.ingresarButton.Name = "ingresarButton";
-            this.ingresarButton.Size = new System.Drawing.Size(100, 28);
+            this.ingresarButton.Size = new System.Drawing.Size(75, 23);
             this.ingresarButton.TabIndex = 4;
             this.ingresarButton.Text = "Ingresar";
             this.ingresarButton.UseVisualStyleBackColor = true;
@@ -91,10 +87,9 @@
             // 
             // RegistrarseB
             // 
-            this.RegistrarseB.Location = new System.Drawing.Point(333, 294);
-            this.RegistrarseB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RegistrarseB.Location = new System.Drawing.Point(250, 239);
             this.RegistrarseB.Name = "RegistrarseB";
-            this.RegistrarseB.Size = new System.Drawing.Size(100, 28);
+            this.RegistrarseB.Size = new System.Drawing.Size(75, 23);
             this.RegistrarseB.TabIndex = 5;
             this.RegistrarseB.Text = "Registrarse";
             this.RegistrarseB.UseVisualStyleBackColor = true;
@@ -104,19 +99,20 @@
             // 
             this.Titulo.AutoSize = true;
             this.Titulo.Font = new System.Drawing.Font("Rockwell", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Titulo.Location = new System.Drawing.Point(66, 47);
+            this.Titulo.Location = new System.Drawing.Point(50, 38);
+            this.Titulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(624, 68);
+            this.Titulo.Size = new System.Drawing.Size(499, 54);
             this.Titulo.TabIndex = 6;
             this.Titulo.Text = "Simulador de Horario";
             this.Titulo.Click += new System.EventHandler(this.Form1_Load);
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(745, 441);
+            this.ClientSize = new System.Drawing.Size(559, 358);
             this.Controls.Add(this.Titulo);
             this.Controls.Add(this.RegistrarseB);
             this.Controls.Add(this.ingresarButton);
@@ -125,7 +121,7 @@
             this.Controls.Add(this.usuarioLabel);
             this.Controls.Add(this.usuarioText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "LoginForm";
             this.Text = "Inicio Sesion";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginForm_FormClosing);
--- SimuladorHorario/VistasSimuladorHorario/PlataformaEstudianteForm.Designer.cs
+++ SimuladorHorario/VistasSimuladorHorario/PlataformaEstudianteForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlataformaEstudianteForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.importarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -79,8 +80,7 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1311, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(983, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -92,38 +92,38 @@
             this.guardarToolStripMenuItem,
             this.salirToolStripMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
             this.archivoToolStripMenuItem.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
             // 
             // importarToolStripMenuItem
             // 
             this.importarToolStripMenuItem.Name = "importarToolStripMenuItem";
-            this.importarToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
+            this.importarToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.importarToolStripMenuItem.Text = "Importar...";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(148, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(126, 6);
             // 
             // guardarToolStripMenuItem
             // 
             this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
-            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.guardarToolStripMenuItem.Text = "Guardar...";
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(151, 26);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
             // verToolStripMenuItem
             // 
             this.verToolStripMenuItem.Name = "verToolStripMenuItem";
-            this.verToolStripMenuItem.Size = new System.Drawing.Size(42, 24);
+            this.verToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
             this.verToolStripMenuItem.Text = "Ver";
             // 
             // wIWIToolStripMenuItem
@@ -132,19 +132,19 @@
             this.inscribirCursoToolStripMenuItem,
             this.modificarPerfilToolStripMenuItem});
             this.wIWIToolStripMenuItem.Name = "wIWIToolStripMenuItem";
-            this.wIWIToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.wIWIToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
             this.wIWIToolStripMenuItem.Text = "WIWI";
             // 
             // inscribirCursoToolStripMenuItem
             // 
             this.inscribirCursoToolStripMenuItem.Name = "inscribirCursoToolStripMenuItem";
-            this.inscribirCursoToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.inscribirCursoToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.inscribirCursoToolStripMenuItem.Text = "Inscribir Curso...";
             // 
             // modificarPerfilToolStripMenuItem
             // 
             this.modificarPerfilToolStripMenuItem.Name = "modificarPerfilToolStripMenuItem";
-            this.modificarPerfilToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.modificarPerfilToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
             this.modificarPerfilToolStripMenuItem.Text = "Modificar Perfil...";
             // 
             // ayudaToolStripMenuItem
@@ -153,27 +153,26 @@
             this.documentacionToolStripMenuItem,
             this.acercaDeSHToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
             // documentacionToolStripMenuItem
             // 
             this.documentacionToolStripMenuItem.Name = "documentacionToolStripMenuItem";
-            this.documentacionToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.documentacionToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
             this.documentacionToolStripMenuItem.Text = "Documentacion";
             // 
             // acercaDeSHToolStripMenuItem
             // 
             this.acercaDeSHToolStripMenuItem.Name = "acercaDeSHToolStripMenuItem";
-            this.acercaDeSHToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.acercaDeSHToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
             this.acercaDeSHToolStripMenuItem.Text = "Acerca de SH";
             // 
             // splitContainer1
             // 
             this.splitContainer1.BackColor = System.Drawing.Color.MintCream;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -183,9 +182,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Size = new System.Drawing.Size(1311, 636);
-            this.splitContainer1.SplitterDistance = 436;
-            this.splitContainer1.SplitterWidth = 11;
+            this.splitContainer1.Size = new System.Drawing.Size(983, 516);
+            this.splitContainer1.SplitterDistance = 326;
+            this.splitContainer1.SplitterWidth = 8;
             this.splitContainer1.TabIndex = 1;
             // 
             // AgendaDataGrid
@@ -197,12 +196,11 @@
             this.Agenda});
             this.AgendaDataGrid.Dock = System.Windows.Forms.DockStyle.Top;
             this.AgendaDataGrid.Location = new System.Drawing.Point(0, 0);
-            this.AgendaDataGrid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AgendaDataGrid.Name = "AgendaDataGrid";
             this.AgendaDataGrid.ReadOnly = true;
             this.AgendaDataGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
             this.AgendaDataGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.AgendaDataGrid.Size = new System.Drawing.Size(436, 108);
+            this.AgendaDataGrid.Size = new System.Drawing.Size(326, 88);
             this.AgendaDataGrid.TabIndex = 0;
             this.AgendaDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
@@ -228,10 +226,9 @@
             this.Domingo});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(864, 636);
+            this.dataGridView1.Size = new System.Drawing.Size(649, 516);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
@@ -297,12 +294,12 @@
             // 
             // PlataformaEstudianteForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1311, 664);
+            this.ClientSize = new System.Drawing.Size(983, 540);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PlataformaEstudianteForm";
             this.Text = "Plataforma Estudiante";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PlataformaEstudianteForm_FormClosing);
